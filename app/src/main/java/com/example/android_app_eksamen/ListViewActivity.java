528d499a109661337c2bc0f4d3f373dbd0c186d1
Nataniel <nataniel.haraldseth@hotmail.com>
2019-11-19 12:54:37 +0100

    Add basic ListView logic + layout resources

--- app/src/main/java/com/example/android_app_eksamen/ListViewActivity.java
+++ app/src/main/java/com/example/android_app_eksamen/ListViewActivity.java
@@ -1,14 +1,49 @@
 package com.example.android_app_eksamen;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import java.util.ArrayList;
 public class ListViewActivity extends AppCompatActivity {
+
+    //
+    private ListView list;
+    private ArrayList<String> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
+        //
+        list = findViewById(R.id.list);
+        //
+        arrayList = new ArrayList<>();
+        arrayList.add("JAVA");
+        arrayList.add("ANDROID");
+        arrayList.add("C Language");
+        arrayList.add("CPP Language");
+        arrayList.add("Go Language");
+        arrayList.add("AVN SYSTEMS");
+        arrayList.add("JAVA");
+        arrayList.add("ANDROID");
+        arrayList.add("C Language");
+        arrayList.add("CPP Language");
+        arrayList.add("Go Language");
+        arrayList.add("AVN SYSTEMS");
+        arrayList.add("JAVA");
+        arrayList.add("ANDROID");
+        arrayList.add("C Language");
+        arrayList.add("CPP Language");
+        arrayList.add("Go Language");
+        arrayList.add("AVN SYSTEMS");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
+        list.setAdapter(arrayAdapter);
     }
 }
