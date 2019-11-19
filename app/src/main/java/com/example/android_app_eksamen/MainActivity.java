cde4b77d5db100de47092dd0976587978383cf8f
Nataniel <nataniel.haraldseth@hotmail.com>
2019-11-19 09:47:51 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android_app_eksamen/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.android_app_eksamen;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Variabler for RecyclerViewet
+    private LinkedList<String> mOrdListe = new LinkedList<>();
+    private RecyclerView mRecyclerView;
+    private RecyclerViewAdapter mAdapter;
+
+    //Variabler for toolbar
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //
+        toolbar         = findViewById(R.id.toolbar);
+        mRecyclerView   = findViewById(R.id.recycler_view);
+
+        //
+        setSupportActionBar(toolbar);
+
+        //
+        populerListe();
+        mAdapter = new RecyclerViewAdapter(this, mOrdListe);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    }/**SLUTT PÅ OnCreate*/
+
+    private void populerListe() {
+        for (int i = 0; i <= 20; i++) {
+            mOrdListe.add("Numero: " + i);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}/**SLUTT PÅ KLASSE MainActivity*/
---
+++ app/src/main/java/com/example/android_app_eksamen/RecyclerViewAdapter.java
@@ -0,0 +1,61 @@
+package com.example.android_app_eksamen;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.LinkedList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.WordViewHolder> {
+
+    private final LinkedList<String> mOrdListe;
+    private LayoutInflater mBlåsOpp;
+
+    public RecyclerViewAdapter(Context context, LinkedList<String> mWordList) {
+        mBlåsOpp = LayoutInflater.from(context);
+        this.mOrdListe = mWordList;
+    }
+
+    @Override
+    public RecyclerViewAdapter.WordViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View mItemView = mBlåsOpp.inflate(R.layout.recyclerview_item, parent, false);
+        return new WordViewHolder(mItemView, this);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewAdapter.WordViewHolder holder, int position) {
+        String current = mOrdListe.get(position);
+        holder.wordItemView.setText(current);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mOrdListe.size();
+    }
+
+    public class WordViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public final TextView wordItemView;
+        final RecyclerViewAdapter mAdapter;
+
+        public WordViewHolder(View itemView, RecyclerViewAdapter adapter) {
+            super(itemView);
+            wordItemView = itemView.findViewById(R.id.ord);
+            this.mAdapter = adapter;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int mPosition = getLayoutPosition();
+            String element = mOrdListe.get(mPosition);
+            mOrdListe.set(mPosition, "clicked " + element);
+            mAdapter.notifyDataSetChanged();
+        }
+    }/**SLUTT PÅ WordViewHolder KLASSE*/
+
+}/**SLUTT PÅ RecyclerViewAdapter KLASSE*/
