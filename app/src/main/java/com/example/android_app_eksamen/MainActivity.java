179d0d0e54b531c8420ca4f5dac5122de3ae6111
Nataniel <nataniel.haraldseth@hotmail.com>
2019-11-19 13:47:02 +0100

    Add layout for setting + intent to get there + minor edits.

--- app/src/main/java/com/example/android_app_eksamen/MainActivity.java
+++ app/src/main/java/com/example/android_app_eksamen/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android_app_eksamen;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -65,9 +66,10 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent startIntent = new Intent(MainActivity.this, SettingActivity.class);
+            startActivity(startIntent);
             return true;
         }
 
